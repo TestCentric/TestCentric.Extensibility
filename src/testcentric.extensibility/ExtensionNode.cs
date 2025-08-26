d9250a18c7606bd786eb634da2a1f8925d8a4f1a
Charlie Poole <charliepoole@gmail.com>
2025-08-26 11:13:55 -0700

    Initialize extensions to Unloaded status

--- src/testcentric.extensibility/ExtensionNode.cs
+++ src/testcentric.extensibility/ExtensionNode.cs
@@ -35,7 +35,7 @@ namespace TestCentric.Extensibility
             AssemblyPath = assemblyPath;
             AssemblyVersion = assemblyVersion;
             TypeName = typeName;
-            Status = ExtensionStatus.Error; // Unloaded;
+            Status = ExtensionStatus.Unloaded;
             Enabled = true; // By default
         }
 
